e1a0de448f36191e455d536fd74055e69289867d
metinsay <metinnsay@gmail.com>
2017-12-13 17:11:56 -0500

    Changed prepare and Added lstm and evaluation

---
+++ lstm.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from prepare import *
+import tqdm
+
+class QA_LSTM(nn.Module):
+
+    def __init__(self, num_hidden, emb_size, embeddings, dropout_prob):
+        super(QA_LSTM, self).__init__()
+        self.num_hidden = num_hidden
+        self.emb_size = emb_size
+        self.embeddings = embeddings
+
+        self.lstm = nn.LSTM(emb_size, num_hidden)
+        self.dropout = nn.Dropout(dropout_prob)
+
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        return (autograd.Variable(torch.zeros(1, 1, self.num_hidden)),
+                autograd.Variable(torch.zeros(1, 1, self.num_hidden)))
+
+    def forward(self, x):
+        self.embedding_layer = nn.Embedding(x.shape()[0], self.emb_size)
+        self.embedding_layer.weight.data = torch.from_numpy(self.embeddings)
+        x = self.embedding_layer(x)
+        lstm_out, self.hidden = self.lstm(x)
+        out = self.dropout(lstm_out)
+        return out
+
+
+
+def model_size():
+    net = QA_LSTM()
+    params = list(net.parameters())
+    total = 0
+    for param in params:
+        this = 1
+        for p in param.size():
+            this *= p
+        total += this
+    return total
+
+
+def run_model(data, model, is_training, params):
+     # Does the update
+    learning_rate = params['learning_rate']
+    margin = params['margin']
+    momentum = params['momentum']
+    num_epoches = params['num_epoches']
+
+    loss_func = nn.MarginRankingLoss(margin)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+
+    for epoch in tqdm.tqdm(range(num_epoches)):  # loop over the dataset multiple times
+        print "Running epoch:", epoch+1
+        run_epoch(data, model, optimizer, is_training, params)
+        if i % 5 == 0:
+            torch.save(net, 'mytraining{}.pt'.format(i))
+    torch.save(net, 'mytraininglast.pt')
+
+def run_epoch(data, model, optimizer, is_training, params):
+
+    batch_size = params['batch_size']
+    loader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
+    running_loss = 0.0
+    for i, data in enumerate(loader):
+        # get the inputs
+        question, candidate_set = data
+
+        # wrap them in Variable
+        question_title, question_body = Variable(question[0]), Variable(question[1])
+
+        question_title, question_body = Variable(question[0]), Variable(question[1])
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        output1 = net(input1).float()
+        output2 = net(input2).float()
+        # print output1, output2, labels
+        # labels = Variable(torch.ones(output1.size()[1]))
+        loss = criterion(output1, output2, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.data[0]
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+def main():
+
+    batch_size = 32
+    dropout_prob = 0.1
+    learning_rate = 1e-3
+    margin = 0.2
+    momentum = 0.9
+    num_epoches = 50
+    num_hidden = 400
+    emb_size = 200
+    is_training = True
+
+    params = {'num_epoches' : num_epoches, 'momentum' : momentum,
+                'margin' : margin, 'learning_rate' : learning_rate,
+                'batch_size' : batch_size}
+
+    data = UbuntuSequentialDataSet()
+
+
+
+    model = QA_LSTM(num_hidden, emb_size, embeddings, dropout_prob)
+
+    print('Started Training...\n')
+
+    run_model(data, model, is_training, params)
+
+    print('\nFinished Training!\n')
+
+if __name__ == '__main__':
+    main()
+    # model_size()
--- prepare.py
+++ prepare.py
@@ -3,39 +3,71 @@ import gzip
 import numpy as np
 import torch
 import torch.utils.data as d
+from torch import Tensor
 import time
+import tqdm
+import random
 
 start = time.time()
 
+NUM_NEGATIVE_SAMPLES = 20
+LEN_EMBEDDING = 200
+
 def load_word_vector():
     embedding_path = 'vector/vectors_pruned.200.txt.gz'
-    embeddings = {}
+    embeddings = [np.zeros(LEN_EMBEDDING)]
+    word_to_index = {}
+
+    print "Embeddings Loading..."
+
     with gzip.open(embedding_path) as file:
-        for line in file:
+        for i,line in tqdm.tqdm(enumerate(file)):
             line = line.split()
             word = line[0]
             vec = np.array(map(float, line[1:]), dtype='float64')
-            embeddings[word] = vec
-    return embeddings
+            embeddings.append(vec)
+            word_to_index[word] = i + 1
+
+    print "Embeddings Loaded! \n"
+
+    return np.array(embeddings), word_to_index
 
 
-def load_tokenized_text(fname):
+embeddings, word_to_index = load_word_vector()
+
+def get_word_index(word):
+    if word in word_to_index:
+        return word_to_index[word]
+    else:
+        return 0
+
+def load_tokenized_text(fname, name, body_trim_length=100):
     '''Creates dictionary of {id: (title, body)}
     This is okay to have in memory since it's just like 160MB or so...
     '''
+    print name + " Question Text Loading..."
+
     data = {}
     with gzip.open(fname) as f:
-        for line in f:
+        for line in tqdm.tqdm(f):
             sections = line.strip().split('\t')
-            body = sections[2] if len(sections) > 2 else ''
-            data[sections[0]] = (sections[1], body)
+            qid, title, body  = sections[0], sections[1], sections[2] if len(sections) > 2 else ''
+
+            body = " ".join(body.split(' ')[:body_trim_length]) # Trims body to 100 words
+            body = [get_word_index(word) for word in body]
+
+            title = [get_word_index(word) for word in sections[1]]
+
+            data[qid] = (title, body)
+
+    print name + " Question Text Loaded! \n"
     return data
 
 
 # use as global variable for reading from
-embeddings = load_word_vector()
-ubuntu_data = load_tokenized_text('ubuntu_data/text_tokenized.txt.gz')
-android_data = load_tokenized_text('android_data/corpus.tsv.gz')
+
+ubuntu_data = load_tokenized_text('ubuntu_data/text_tokenized.txt.gz', 'Ubuntu')
+# android_data = load_tokenized_text('android_data/corpus.tsv.gz', 'Android')
 
 
 def load_ubuntu_examples(dev=False, test=False):
@@ -46,96 +78,85 @@ def load_ubuntu_examples(dev=False, test=False):
         file = 'ubuntu_data/dev.txt'
     elif test:
         file = 'ubuntu_data/test.txt'
+
+    print "Loading Data From " + file + "..."
+
     count = 0
     with open(file) as f:
-        for line in f:
+        for line in tqdm.tqdm(f):
             count += 1
             query, positive, negative = map(lambda x: x.split(), line.split('\t'))
             query = query[0]
-            p = ubuntu_data[positive[0]]
-            q = ubuntu_data[query]
-            for n in negative:
-                yield (q, p, ubuntu_data[n])
+            for p in positive:
+                yield (query, p, negative)
+
+    print "Loaded Data From " + file + "! \n"
+
+# def load_android_examples(dev=False, test=False):
+#     '''yields data in the form of
+#     (id, query (title, body), example (title, body), +1/-1)'''
+#     assert dev or test  # can only be dev or test sets
+#     neg_file = 'android_data/dev.neg.txt'
+#     pos_file = 'android_data/dev.pos.txt'
+#     if test:
+#         neg_file = 'android_data/test.neg.txt'
+#         pos_file = 'android_data/test.pos.txt'
+#     with open(neg_file) as f:
+#         for line in f:
+#             query, compare = line.split()
+#             yield (query, android_data[query], android_data[compare], -1)
+#     with open(pos_file) as f:
+#         for line in f:
+#             query, compare = line.split()
+#             yield (query, android_data[query], android_data[compare], 1)
 
 
-def load_android_examples(dev=False, test=False):
-    '''yields data in the form of
-    (id, query (title, body), example (title, body), +1/-1)'''
-    assert dev or test  # can only be dev or test sets
-    neg_file = 'android_data/dev.neg.txt'
-    pos_file = 'android_data/dev.pos.txt'
-    if test:
-        neg_file = 'android_data/test.neg.txt'
-        pos_file = 'android_data/test.pos.txt'
-    with open(neg_file) as f:
-        for line in f:
-            query, compare = line.split()
-            yield (query, android_data[query], android_data[compare], -1)
-    with open(pos_file) as f:
-        for line in f:
-            query, compare = line.split()
-            yield (query, android_data[query], android_data[compare], 1)
+# I DON'T THINK WE NEED THIS BUT YOU DECIDE
 
-
-def example_to_word_vector(query, pos, neg):
-    query = ''.join(query).split()
-    pos = ''.join(pos).split()
-    neg = ''.join(neg).split()
-
-    query_vec = np.zeros(200)
-    pos_vec = np.zeros(200)
-    neg_vec = np.zeros(200)
-    count = 0
-    for word in query:
-        if word in embeddings:
-            count += 1
-            query_vec += embeddings[word]
-    if count > 0:
-        query_vec = query_vec/count
-        count = 0
-    for word in pos:
-        if word in embeddings:
-            pos_vec += embeddings[word]
-            count += 1
-    if count > 0:
-        pos_vec = pos_vec/count
-        count = 0
-    for word in neg:
-        if word in embeddings:
-            neg_vec += embeddings[word]
-            count += 1
-    if count > 0:
-        neg_vec = neg_vec/count
-    return (query_vec, pos_vec, neg_vec)
-
-def example_to_seq_word_vector(qid, query, example, label):
-    query = ''.join(query).split()
-    example = ''.join(example).split()
-
-    query_embeddings = []
-    example_embeddings = []
-
-    for word in query:
-        if word in embeddings:
-            query_embeddings.append(embeddings[word])
-    for word in example:
-        if word in embeddings:
-            example_embeddings.append(embeddings[word])
-    return (qid, query_embeddings, example_embeddings, label)
-
-
-class UbuntuDataSet(d.Dataset):
-    '''Loads the training set for the Ubuntu Dataset'''
-
-    def __init__(self):
-        self.data = list(load_ubuntu_examples())
-
-    def __getitem__(self, index):
-        (query_vec, pos_vec, neg_vec) = example_to_word_vector(*self.data[index])
-        return torch.from_numpy(np.vstack((query_vec, pos_vec))).float(), torch.from_numpy(np.vstack((query_vec, neg_vec))).float(), 1.0
-
-    def __len__(self):
-        return len(self.data)
+# def example_to_word_vector(query, pos, neg):
+#     query = ''.join(query).split()
+#     pos = ''.join(pos).split()
+#     neg = ''.join(neg).split()
+#
+#     query_vec = np.zeros(200)
+#     pos_vec = np.zeros(200)
+#     neg_vec = np.zeros(200)
+#     count = 0
+#     for word in query:
+#         if word in embeddings:
+#             count += 1
+#             query_vec += embeddings[word]
+#     if count > 0:
+#         query_vec = query_vec/count
+#         count = 0
+#     for word in pos:
+#         if word in embeddings:
+#             pos_vec += embeddings[word]
+#             count += 1
+#     if count > 0:
+#         pos_vec = pos_vec/count
+#         count = 0
+#     for word in neg:
+#         if word in embeddings:
+#             neg_vec += embeddings[word]
+#             count += 1
+#     if count > 0:
+#         neg_vec = neg_vec/count
+#     return (query_vec, pos_vec, neg_vec)
+#
+#
+# class UbuntuDataSet(d.Dataset):
+#     '''Loads the training set for the Ubuntu Dataset'''
+#
+#     def __init__(self):
+#         self.data = list(load_ubuntu_examples())
+#
+#     def __getitem__(self, index):
+#         (query_vec, pos_vec, neg_vec) = example_to_word_vector(*self.data[index])
+#         return torch.from_numpy(np.vstack((query_vec, pos_vec))).float(), torch.from_numpy(np.vstack((query_vec, neg_vec))).float(), 1.0
+#
+#     def __len__(self):
+#         return len(self.data)
 
 
 class UbuntuSequentialDataSet(d.Dataset):
@@ -145,17 +166,22 @@ class UbuntuSequentialDataSet(d.Dataset):
         self.data = list(load_ubuntu_examples())
 
     def __getitem__(self, index):
-        # query_vecs and example_vecs are lists consisting of items that are length 200 word vectors
-        (qid, query_vecs, example_vecs, label) = example_to_seq_word_vector(*self.data[index])
-        # return torch.from_numpy(np.concatenate((query_vec, example_vec))), label
-        # TODO
-        raise NotImplementedError
+        qid, pid, nids = self.data[index]
+
+        neg_samples = random.sample(nids, NUM_NEGATIVE_SAMPLES)
+        candidate_set = [pid] + neg_samples
+
+        q = ubuntu_data[qid]
+        c = [ubuntu_data[i] for i in candidate_set]
+        l = [1] + [0] * len(nids)
+
+        return q, c, l
+
 
     def __len__(self):
         return len(self.data)
 
 
 if __name__ == '__main__':
-    print "initial load_time:", time.time()-start
-    s = UbuntuDataSet()
+    s = UbuntuSequentialDataSet()
     print "total time:", time.time()-start
